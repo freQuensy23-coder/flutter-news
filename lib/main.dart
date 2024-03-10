af6bb6557c4222814d7505e130d300278c349cd2
alexeymametyev <mametevalex@gmail.com>
2024-03-11 00:00:32 +0300

    [IMP] dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/theme_notifier.dart';
 import 'news_scrollable.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const App());
 
-class App extends StatelessWidget {
-  const App({super.key});
+void main() => runApp(const NewsApp());
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('News')),
-        body: const NewsScrollableListWidget(),
-      ),
-      theme: ThemeData(
-        // Define the default brightness and colors.
-        brightness: Brightness.dark,
-        primaryColor: Colors.lightBlue[800],
-        // Define the default font family.
-        fontFamily: 'Georgia',
-      ),
+    return ChangeNotifierProvider(
+      create: (_) => ThemeNotifier(ThemeData.dark()), // Set default theme here
+      child: const NewsAppWithTheme(),
     );
   }
 }
 
+class NewsAppWithTheme extends StatelessWidget {
+  const NewsAppWithTheme({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('News'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.brightness_6),
+              onPressed: () {
+                themeNotifier.setTheme(themeNotifier.getTheme().brightness == Brightness.dark ? ThemeData.light() : ThemeData.dark());
+              },
+            ),
+          ],
+        ),
+        body: const NewsScrollableListWidget(),
+      ),
+      theme: themeNotifier.getTheme(),
+    );
+  }
+}
